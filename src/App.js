fdcd5ed3b7e0d740b6591dffb92e0ba3f218d389
Fabricio <carboni@gmail.com>
2020-06-08 14:53:18 -0400

    timeline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
 import "./App.css";
 import { CssBaseline } from "@material-ui/core";
+
 import Home from "./components";
+import Resume from "./components/Resume";
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <Home />
+      <Route exact path="/" component={Home} />
+      <Route path="/resume" component={Resume} />
     </>
   );
 }
