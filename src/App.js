f70969b5f51af96bb891f02a3fd2e78f9ad72500
Fabricio <carboni@gmail.com>
2020-06-08 08:27:06 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { CssBaseline } from "@material-ui/core";
+import Home from "./components";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Home />
+    </>
+  );
+}
+
+export default App;
